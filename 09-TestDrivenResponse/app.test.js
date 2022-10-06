552e0c210c35b9c3bf305e2a925b7f569d8faa83
Francesca Foglietta <francesfogli@gmail.com>
2022-10-06 13:03:10 +0200

    feat: exercise 09

---
+++ 09-TestDrivenResponse/app.test.js
@@ -0,0 +1,14 @@
+const supertest = require("supertest");
+
+const createApp = require("./app.js");
+
+const app = createApp();
+
+const request = supertest(app);
+
+test("GET /", async () => {
+  const response = await request.get("/")
+    .expect(200)
+    .expect("Content-Type", "text/html")
+    expect(response.text).toEqual("<html><body><h2>Welcome to the World Wide Web!</h2></body></html>");
+});
