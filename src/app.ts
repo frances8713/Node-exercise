42c0d9ba626f6b5d37696665a8cd42f0a99749aa
Francesca Foglietta <francesfogli@gmail.com>
2022-10-19 16:43:21 +0200

    exercise retreive all resources

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
 import express from "express";
 import "express-async-errors"
+import prisma from "./lib/prisma/client";
 
 const app = express();
 
-app.get("/colors", (request, response) => {
-    response.json([
-        { name: "Green"},
-        { name: "Yellow"}
-    ]);
+app.get("/colors", async (request, response) => {
+const colors = await prisma.colors.findMany();
+response.json(colors)
 });
 
 export default app;
--- src/server.test.ts
+++ src/server.test.ts
@@ -1,16 +1,35 @@
 import supertest from "supertest";
 import app from "./app";
+import { prismaMock } from "./lib/prisma/client.mock";
 
-const request = supertest(app)
+const request = supertest(app);
 
-test("GET /colors", async() => {
+test("GET /colors", async () => {
+    const colors = [
+        {
+            "id": 6,
+            "name": "Yellow",
+            "description": null,
+            "createAt": "2022-10-19T14:02:16.861Z",
+            "updateAt": "2022-10-19T14:02:02.610Z"
+        },
+        {
+            "id": 7,
+            "name": "Green",
+            "description": null,
+            "createAt": "2022-10-19T14:02:16.861Z",
+            "updateAt": "2022-10-19T14:02:06.822Z"
+        }
+    ];
+
+    //@ts-ignore
+    prismaMock.colors.findMany.mockResolvedValue(colors);
+
     const response = await request
-    .get("/colors")
-    .expect(200)
-    .expect("Content-Type", /application\/json/)
+        .get("/colors")
+        .expect(200)
+        .expect("Content-Type", /application\/json/);
 
-    expect(response.body).toEqual ([
-        { name: "Green"},
-        { name: "Yellow"}
-    ])
+    expect(response.body).toEqual(colors);
 });
+
