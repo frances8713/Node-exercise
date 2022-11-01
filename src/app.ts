508f1712f3fd9c88934551529c2d87f9e157691b
Francesca Foglietta <francesfogli@gmail.com>
2022-11-01 18:09:08 +0100

    implement passport

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,15 @@ import "express-async-errors";
 import { validationErrorMiddleware } from "./lib/middleware/validation";
 import colorsRoutes from "./routes/colors";
 import { initCorseMiddleware } from "./lib/middleware/cors";
+import { initSessionMiddleware } from "./lib/middleware/session";
+import { passport } from "./lib/middleware/passport";
 
 
 const app = express();
+
+app.use(initSessionMiddleware());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.json());
 
